67c66c8aa7b242530464b91545f6a878d7042d4f
Ethan Tecson <ethantecson@gmail.com>
2024-04-17 23:14:32 -0400

    BST passed Truex testing

--- graph.cpp
+++ graph.cpp
@@ -1,130 +1,218 @@
 #include "graph.h"
 #include <sstream>
+#include <queue>
+
 
 //===================================
 // Vertex Class
 //===================================
 
 
+
+
 /**
- * @brief Empty Constructor for Vertex Class
- *
- * Creates a Vertex Object with no values for key or data
- *
- * @param none
- *
- * @note Pre-Condition: none
- * @note Post-Condition: Creates a Node object
- *
- * @returns none
- */
+* @brief Empty Constructor for Vertex Class
+*
+* Creates a Vertex Object with no values for key or data
+*
+* @param none
+*
+* @note Pre-Condition: none
+* @note Post-Condition: Creates a Node object
+*
+* @returns none
+*/
 template <typename Data, typename Key>
 Vertex<Data,Key>::Vertex(){
-    parent = nullptr;
-    color_white = true;
-    distance = -1;
+   parent = nullptr;
+   color_white = true;
+   distance = -1;
+   adjacencies_list = vector<Vertex<Data,Key>*>();
 }
 
+
 /**
- * @brief Constructor with values for Vertex Class
- *
- * Creates a Vertex Object with the data and key attributes equal 
- * to the data and key given in the argument
- *
- * @param d - data for vertex object to be initiated with
- * @param k - key for vertex object to be initiated with
- *s
- * @note Pre-Condition: none
- * @note Post-Condition: Creates a Vertex object 
- *
- * @returns none
- */
+* @brief Constructor with values for Vertex Class
+*
+* Creates a Vertex Object with the data and key attributes equal
+* to the data and key given in the argument
+*
+* @param d - data for vertex object to be initiated with
+* @param k - key for vertex object to be initiated with
+*s
+* @note Pre-Condition: none
+* @note Post-Condition: Creates a Vertex object
+*
+* @returns none
+*/
 template <typename Data, typename Key>
 Vertex<Data,Key>::Vertex(Data d, Key k){
-    parent = nullptr;
-    color_white = true;
-    distance = -1;
-    data = d;
-    key = k;
+   parent = nullptr;
+   color_white = true;
+   distance = -1;
+   data = d;
+   key = k;
+   adjacencies_list = vector<Vertex<Data,Key>*>();
 }
 
+
 /**
- * @brief Deconstructor for Vertex Class
- *
- * Deallocates memory created for vertex objects
- *
- *@param none
- * 
- * @note Pre-Condition: none
- * @note Post-Condition:a All vertex objects deallocated
- *
- * @returns none
- */
+* @brief Deconstructor for Vertex Class
+*
+* Deallocates memory created for vertex objects
+*
+*@param none
+*
+* @note Pre-Condition: none
+* @note Post-Condition:a All vertex objects deallocated
+*
+* @returns none
+*/
 template <typename Data, typename Key>
 Vertex<Data, Key>::~Vertex() {
-    // Nothing needed
-    // Will probably need to have a heper function that deletes nodes that 
-    // will be used in the Graph deconstructor
+   // Nothing needed
+   // Will probably need to have a heper function that deletes nodes that
+   // will be used in the Graph deconstructor
 }
+
 
 //===================================
 // Graph Class
 //===================================
 
+
 /**
- * @brief Parameter Constructor for Graph Class
- *
- * Creates graph object with parameter keys, data, and adjacencies
- *
- * @param keys vector, data vector, adjacencies vector of vectors
- * 
- * @note Pre-Condition: none
- * @note Post-Condition: creates a Graph object
- *
- * @returns none
- */
+* @brief Parameter Constructor for Graph Class
+*
+* Creates graph object with parameter keys, data, and adjacencies
+*
+* @param keys vector, data vector, adjacencies vector of vectors
+*
+* @note Pre-Condition: none
+* @note Post-Condition: creates a Graph object
+*
+* @returns none
+*/
 template <typename Data, typename Key>
 Graph<Data, Key>::Graph(vector<Key> keys, vector<Data> data, vector<vector<Key>> edges) {
-    int len = keys.size();
-    adjacencies.resize(len);
-    
-    for( int i = 0; i < len; i++ ) {
-        Vertex<Data, Key> *new_vertex = new Vertex<Data, Key>(data[i], keys[i]);
-        vertices.push_back(new_vertex);
-    }
+   int len = keys.size();
+   adjacencies.resize(len);
+  
+   // Load graph->vertices with new vertexes
+   for( int i = 0; i < len; i++ ) {
+       Vertex<Data, Key> *new_vertex = new Vertex<Data, Key>(data[i], keys[i]);
+       vertices.push_back(new_vertex);
+   }
 
-    for( int j = 0; j < len; j++ ) {
-        int edges_len = edges[j].size();
-        for( int k = 0; k < edges_len; k++) {
-            Vertex<Data, Key> *new_ptr = get(edges[j][k]);
-            adjacencies[j].push_back(new_ptr);
-        }
-    }
+
+   // Vertex adjacencies_list
+   for ( int i = 0; i < edges.size(); i++ ){
+       Vertex<Data,Key>* current_vertex = get(keys[i]);
+       for ( int j = 0; j < edges[i].size(); j++){
+           current_vertex->adjacencies_list.push_back(get(edges[i][j]));
+       }
+   }
+
+
+   // Adjacencies
+   for( int j = 0; j < len; j++ ) {
+       int edges_len = edges[j].size();
+       for( int k = 0; k < edges_len; k++) {
+           Vertex<Data, Key> *new_ptr = get(edges[j][k]);
+           adjacencies[j].push_back(new_ptr);
+       }
+   }
+}
+
+
+
+
+/**
+* @brief get
+*
+* returns the pointer to the vertex with key k
+*
+* @param key k
+*
+* @note Pre-Condition: Graph is a valid graph object
+* @note Post-Condition: no changes, but returns a pointer to the vertex with key k
+*
+* @returns none
+*/
+template <typename Data, typename Key>
+Vertex<Data, Key>* Graph<Data, Key>::get(Key k) const {
+   int len = vertices.size();
+
+
+   for( int i = 0; i < len; i++ ) {
+       if ( vertices[i]->key == k )
+           return vertices[i];
+   }
+
+
+   return nullptr;
 }
 
 
 /**
- * @brief get 
- *
- * returns the pointer to the vertex with key k
- *
- * @param key k
- * 
- * @note Pre-Condition: Graph is a valid graph object
- * @note Post-Condition: no changes, but returns a pointer to the vertex with key k
- *
- * @returns none
- */
+* @brief bfs
+*
+* performs a breadth-first search on the graph, starting at source s
+*
+* @param source s, of type Key
+*
+* @note Pre-Condition: none
+* @note Post-Condition: performs a bfs on the graph
+*
+* @returns none, but changes the attributes of the vertices
+*/
+
+
 template <typename Data, typename Key>
-//typename Graph<Data, Key>::Vertex* Graph<Data, Key>::get(Key k) const{
-Vertex<Data, Key>* Graph<Data, Key>::get(Key k) const {
-    int len = vertices.size();
-    //Vertex<Data, Key> *ret = nullptr;
+void Graph<Data, Key>::bfs(Key source) const {
 
-    for( int i = 0; i < len; i++ ) {
-        if ( vertices[i]->key == k )
-            return vertices[i];
-    }
 
-    return nullptr;
+   int len = vertices.size();
+   for( int i = 0; i < len; i++ ) {
+       vertices[i]->color_white = true;
+       vertices[i]->distance = -1;
+       vertices[i]->parent = nullptr;
+   }
+
+
+   Vertex<Data, Key> *s = get(source);
+   s->color_white = false;
+   s->distance = 0;
+   s->parent = nullptr;
+
+
+   queue<Vertex<Data, Key>*> Q;
+   Q.push(s);
+
+
+   while( !Q.empty() ) {
+       Vertex<Data, Key> *u = Q.front();
+       Q.pop();
+
+
+       int adj_len = u->adjacencies_list.size();
+
+
+       vector<Vertex<Data, Key>*> *adj_list_ptr = &u->adjacencies_list;
+
+
+       for( int j = 0; j < adj_len; j++ ) {
+           if ( (*adj_list_ptr)[j]->color_white ) {
+               (*adj_list_ptr)[j]->color_white = false;
+               (*adj_list_ptr)[j]->distance = u->distance + 1;
+               (*adj_list_ptr)[j]->parent = u;
+
+
+               Q.push( (*adj_list_ptr)[j] );
+           }
+
+
+           u->color_white = false;
+       }
+   }
 }
--- graph.h
+++ graph.h
@@ -5,51 +5,63 @@
 // Ethan Tecson, Nhien Hong, Liam Qiunlan        //
 //-----------------------------------------------//
 
+
 #ifndef GRAPH_H
 #define GRAPH_H
+
 
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 
+
 template <typename Data, typename Key>
 class Graph;
 
+
 template <typename Data, typename Key>
 class Vertex {
-    private:
-        bool color_white;
-        Vertex* parent; 
-        int distance;
-        friend class Graph<Data, Key>;
-        
-    public:
-        Data data; 
-        Key key;
+   private:
+       bool color_white;
+       Vertex<Data, Key>* parent;
+       vector<Vertex<Data, Key>*> adjacencies_list;
+       friend class Graph<Data, Key>;
+      
+   public:
+       Data data;
+       Key key;
+       int distance;
 
-        Vertex();
-        Vertex(Data d, Key k);
-        ~Vertex();
-        // May need heper function for deleting nodes
+
+       Vertex();
+       Vertex(Data d, Key k);
+       ~Vertex();
+       // May need heper function for deleting nodes
 };
+
 
 template <typename Data, typename Key>
 class Graph {
-    private:
-        vector<Vertex<Data,Key>*> vertices;
-        vector<vector<Vertex<Data, Key>*>> adjacencies;
+   private:
+       vector<Vertex<Data,Key>*> vertices;
+       vector<vector<Vertex<Data, Key>*>> adjacencies;
 
-    public:
-        Graph();
-        Graph(vector<Key> keys, vector<Data> data, vector<vector<Key>> edges);
-        ~Graph();
-        Vertex<Data, Key>* get(Key k) const;
-        bool reachable(Vertex<Data,Key>* u, Vertex<Data,Key>* v) const;
-        void bfs(Vertex<Data,Key> source) const;
-        string print_path(Key u, Key v) const;
-        string edge_class(Vertex<Data,Key> u, Vertex<Data,Key> v) const;
-        string bfs_tree(Key s) const;
+
+   public:
+       Graph();
+       Graph(vector<Key> keys, vector<Data> data, vector<vector<Key>> edges);
+       ~Graph();
+       Vertex<Data, Key>* get(Key k) const;
+       bool reachable(Vertex<Data,Key>* u, Vertex<Data,Key>* v) const;
+       void bfs(Key source) const;
+       string print_path(Key u, Key v) const;
+       string edge_class(Vertex<Data,Key> u, Vertex<Data,Key> v) const;
+       string bfs_tree(Key s) const;
 };
 
-#endif 
+
+#endif
+
+
+
